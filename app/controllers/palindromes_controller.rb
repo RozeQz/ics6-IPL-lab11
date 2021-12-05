5a12d441b85c73d8d77f085d56cca0150b2b0c14
RozeQz <liza.roz@yandex.ru>
2021-12-05 03:29:40 +0300

    Upgrade controller. No repeated operations

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -10,7 +10,22 @@ class PalindromesController < ApplicationController
 
   # GET /palindromes or /palindromes.json
   def index
-    @palindromes = Palindrome.all
+    @palindromes = Palindrome.last(50)
+
+    respond_to do |format|
+      format.html do
+        render 'index'
+      end
+      format.xml do
+        xml_arr = @palindromes.inject([]) { |acc, el| acc.append el.output }
+        doc_result = Nokogiri::XML('<db></db>')
+        xml_arr.each_with_object(doc_result) do |el, acc|
+          el = Nokogiri::XML(el).search('output')
+          acc.at('db').add_child(el)
+        end
+        render xml: doc_result
+      end
+    end
   end
 
   # GET /palindromes/1 or /palindromes/1.json
@@ -32,15 +47,19 @@ class PalindromesController < ApplicationController
   def create
     @palindrome = Palindrome.new(palindrome_params)
     @palindrome.output = make_output(palindrome_params[:input]) if @palindrome.valid?
-
-    respond_to do |format|
-      if @palindrome.save
-        format.html { redirect_to @palindrome, notice: 'Palindrome was successfully created.' }
-        format.json { render :show, status: :created, location: @palindrome }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @palindrome.errors, status: :unprocessable_entity }
+    found_id = Palindrome.search_id(palindrome_params[:input])
+    if found_id.nil?
+      respond_to do |format|
+        if @palindrome.save
+          format.html { redirect_to @palindrome, notice: 'Palindrome was successfully created.' }
+          format.json { render :show, status: :created, location: @palindrome }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @palindrome.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to action: 'show', id: found_id
     end
   end
 
@@ -80,15 +99,16 @@ class PalindromesController < ApplicationController
 
   def make_output(input)
     return if input.nil? || input.empty?
+    input  = Integer(input)
 
-    numbers = (0..Integer(input)).select { |i| palindrome?(i * i) }
+    numbers = (0..input).select { |i| palindrome?(i * i) }
     result = numbers.size
 
-    palindromes_hash = numbers.each_with_index.map do |number, index|
+    palindromes_hash = numbers.map.with_index do |number, index|
       { "palindrome#{index + 1}": { number: number, square: number**2 } }
     end
 
-    output_hash = { result: result, palindromes: palindromes_hash }
+    output_hash = { result: result || 'Не найдено', palindromes: palindromes_hash }
     output_hash.to_xml.gsub('hash', 'output').gsub(/<palindrome(\d+)>/, '<palindrome i="\1">')
                .gsub(/<palindrome>/, '')
                .gsub(%r{</palindrome(\d+)>}, '')
--- app/models/palindrome.rb
+++ app/models/palindrome.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors.add :input, 'Пустой ввод' if record.input.nil?
+  end
+end
+
 class Palindrome < ApplicationRecord
-  validates :input, presence: true,
+  validates :input, presence: true, uniqueness: true,
                     format: { with: /\A\d+\z/, message: 'Please enter only one number' }
+  validates_with MyValidator, fields: [:input]
+
+  def self.search(search)
+    if search
+      where(input: search)
+    else
+      last(50)
+    end
+  end
+
+  def self.search_id(search)
+    select(:id).find_by(input: search) if search
+  end
 end
